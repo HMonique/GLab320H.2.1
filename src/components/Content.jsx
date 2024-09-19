633260abac81db514d3d38a6c2bb53b7a3ad8435
HMonique <moniquehl@outlook.com>
2024-09-18 22:15:17 -0400

    first commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,21 @@
+function Header() {
+    return (<h1>Simple React Application</h1>);
+  }
+  
+  function Content() {
+    return (<p>I need to practice making React Applications!</p>);
+  }
+  
+  function Footer() {
+    return (<h1>Made by Monique Harris</h1>);
+  }
+  
+  function App() {
+    return (
+      <>
+  
+      </>
+    );
+  }
+  
+  export default App;
